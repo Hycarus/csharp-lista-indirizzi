548bd02d1b99900e776e675223654679b4aca29b
Hycarus <mirkomarasco08@gmail.com>
2024-04-29 16:01:26 +0200

    exercise + bonus completed

---
+++ Indirizzo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace csharp_lista_indirizzi
+{
+	public class Indirizzo
+	{
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public string Street { get; set; }
+		public string City { get; set; }
+		public string Province { get; set; }
+		public int ZIP { get; set; }
+
+		public Indirizzo(string name, string surname, string street, string city, string province, int zip)
+		{
+			Name = name;
+			Surname = surname;
+			Street = street;
+			City = city;
+			Province = province;
+			ZIP = zip;
+		}
+
+        private string CheckField(string field, string fieldName)
+        {
+            if (string.IsNullOrEmpty(field))
+            {
+                return $"{fieldName.ToUpper()} NON DISPONIBILE";
+            }
+            return field;
+        }
+
+        public override string ToString()
+        {
+			string name = CheckField(Name, "Name");
+			string surname = CheckField(Surname, "Surname");
+			string street = CheckField(Street, "Street");
+			string city = CheckField(City, "City");
+			string province = CheckField(Province, "Province");
+
+			return $"{name}, {surname}, {street}, {city}, {province}, ({ZIP})";
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -4,7 +4,69 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string path = "/Users/mirko/boolean/DotNet/csharp-lista-indirizzi/addresses.csv";
+        string path2 = "/Users/mirko/boolean/DotNet/csharp-lista-indirizzi/addresses2.csv";
+        var indirizzi = LeggiDaTesto(path);
+        foreach(var indirizzo in indirizzi)
+        {
+            Console.WriteLine($"Ecco l'indirizzo {indirizzo}");
+        }
+        ScriviInTesto(indirizzi, path2);
+    }
+
+    public static List<Indirizzo> LeggiDaTesto(string path)
+    {
+        List<Indirizzo> indirizzi = new();
+        var stream = File.OpenText(path);
+        int i = 0;
+        while (!stream.EndOfStream)
+        {
+            var linea = stream.ReadLine();
+            i++;
+            if (i <= 1)
+                continue;
+            try
+            {
+                var dati = linea?.Split(',');
+
+                if (dati.Length < 6)
+                {
+                    Console.WriteLine($"Riga {i} non valida: dati mancanti.");
+                }
+
+                string name = dati[0];
+                string surname = dati[1];
+                string street = dati[2];
+                string city = dati[3];
+                string province = dati[4];
+                int zip;
+
+                bool zipConversionSuccess = int.TryParse(dati[5], out zip);
+
+                if (!zipConversionSuccess)
+                {
+                    Console.WriteLine($"Riga {i} contiene un valore di ZIP non valido: '{dati[5]}'.");
+                }
+
+                Indirizzo l = new(name, surname, street, city, province, zip);
+                indirizzi.Add(l);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Errore alla linea {i}: {e.Message}");
+                Console.WriteLine($"Contenuto della linea: {linea}");
+            }
+        }
+
+        stream.Dispose();
+        return indirizzi;
+    }
+
+    public static void ScriviInTesto(List<Indirizzo> indirizzi, string path)
+    {
+        using StreamWriter stream = File.CreateText(path); 
+        foreach (var indirizzo in indirizzi)
+            stream.WriteLine(indirizzo.ToString());
     }
 }
 
